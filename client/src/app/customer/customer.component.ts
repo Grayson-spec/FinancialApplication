cabdd28d325a2b1c0f9a1896321d2d3c2b67700f
Grayson-spec <83462682+Grayson-spec@users.noreply.github.com>
2024-12-19 15:35:37 -0600

    Implemented all components from backup.
    Project is restored now.

--- client/src/app/customer/customer.component.ts
+++ client/src/app/customer/customer.component.ts
@@ -1,5 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { response } from 'express';
+import { SocketAddress } from 'net';
+import { identity } from 'rxjs';
 
+interface Customer {
+  id: number;
+  customerName: string;
+  address: string;
+  contactInformation: string;
+}
 @Component({
   selector: 'app-customer',
   imports: [],
@@ -7,5 +17,34 @@ import { Component } from '@angular/core';
   styleUrl: './customer.component.css'
 })
 export class CustomerComponent {
+  customers: Customer[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getCustomers();
+  }
+
+  getCustomers() {
+    this.http.get<Customer[]>('http://localhost:5235/api/customer')
+    .subscribe(response => {
+      this.customers = response;
+      const tableBody = document.getElementById('customer-table-body');
+      if (tableBody) {
+        this.customers.forEach((customer: Customer) => {
+          const row = `
+          <tr>
+            <td>${customer.id}</td>
+            <td>${customer.customerName}</td>
+            <td>${customer.address}</td>
+            <td>${customer.contactInformation}</td>
+          </tr>
+        `;
+        tableBody.innerHTML += row;
+        });
+      } else {
+        console.error('Table body not found');
+      }
+    });
+  }
 }
--- client/src/app/payment/payment.component.ts
+++ client/src/app/payment/payment.component.ts
@@ -1,4 +1,14 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { response } from 'express';
+
+interface Payment {
+  id: number;
+  paymentDate: Date;
+  paymentMethod: string;
+  paymentAmount: number;
+  invoiceId: number;
+}
 
 @Component({
   selector: 'app-payment',
@@ -6,6 +16,37 @@ import { Component } from '@angular/core';
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
 
+export class PaymentComponent {
+  payments: Payment[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getPayments();
+  }
+
+  getPayments() {
+    this.http.get<Payment[]>('http://localhost:5235/api/payment')
+    .subscribe(response => {
+      this.payments = response;
+      const tableBody = document.getElementById('payment-history-table');
+      if (tableBody) {
+        this.payments.forEach((payment: Payment) => {
+          const row = `
+          <tr>
+            <td>${payment.id}</td>
+            <td>${payment.paymentDate}</td>
+            <td>${payment.paymentMethod}</td>
+            <td>${payment.paymentAmount}</td>
+            <td>${payment.invoiceId}</td>
+          </tr>
+          `;
+          tableBody.innerHTML += row;
+        });
+      } else {
+        console.error('Table body not found');
+      }
+    });
+  }
 }
